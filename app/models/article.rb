f78c96e90686099957e2a06cfe97ec84fff3108c
首藤　勝史 <katsu19971997@gmail.com>
2022-04-07 23:39:23 +0900

    viewの変更

--- app/models/article.rb
+++ app/models/article.rb
@@ -21,10 +21,10 @@ class Article < ApplicationRecord
     validates :title, format: { with: /\A(?!\@)/}
 
     validates :content, presence: true
-    validates :content, length: { minimum: 10 }
-    validates :content, uniqueness: true
+    # validates :content, length: { minimum: 10 }
+    # validates :content, uniqueness: true
 
-    validate :validate_title_and_content_length
+    # validate :validate_title_and_content_length
 
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
@@ -42,9 +42,9 @@ class Article < ApplicationRecord
         likes.count
     end
 
-    private
-    def validate_title_and_content_length
-      char_count = self.title.length + self.content.length
-      errors.add(:content, '100文字いじょうで！') unless char_count > 100
-    end
+    # private
+    # def validate_title_and_content_length
+    #   char_count = self.title.length + self.content.length
+    #   errors.add(:content, '100文字いじょうで！') unless char_count > 100
+    # end
 end
